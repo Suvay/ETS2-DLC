6c70002f85ad09cd2392f798799deecb7bece2f4
Suvay <90697523+Suvay@users.noreply.github.com>
2025-05-09 15:24:19 +0800

    Update INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,60 +1,38 @@
 SETLOCAL ENABLEEXTENSIONS
 
 :: === CONFIG ===
-SET "RAR_NAME=ETS2-ALL-DLCs-v1.54.1.0_Mosbymods.de.rar"
-SET "RAR_PASS=mosbymods.de"
-SET "DOWNLOAD_LINK=https://www.dropbox.com/scl/fi/qg0ueewnfdaz143tyxa36/ETS2-ALL-DLCs-v1.54.1.0_Mosbymods.de.rar?rlkey=46balkkoytvhvcvnpzclij8wp&st=g99d4456&dl=1"
-SET "WORKDIR=%USERPROFILE%\Downloads\ETS2Extracted"
+SET "EXTRACTED_DIR=%USERPROFILE%\Downloads\ETS2Extracted"  :: Set the folder where the extracted files are located
 SET "TARGET=F:\SteamLibrary\steamapps\common\Euro Truck Simulator 2"
-SET "7Z=%ProgramFiles%\7-Zip\7z.exe"
-
-:: === CREATE WORKDIR ===
-IF NOT EXIST "%WORKDIR%" mkdir "%WORKDIR%"
-cd /d "%WORKDIR%"
-
-:: === DOWNLOAD FILE WITH PROGRESS BAR ===
-echo.
-echo === DOWNLOADING %RAR_NAME% ===
-curl -# -L -o "%RAR_NAME%" "%DOWNLOAD_LINK%"
-
-IF NOT EXIST "%RAR_NAME%" (
-    echo.
-    echo [ERROR] Failed to download file.
-    pause
-    exit /b
-)
-
-:: === EXTRACT RAR FILE ===
-echo.
-echo === EXTRACTING %RAR_NAME% ===
-"%7Z%" x "%RAR_NAME%" -p%RAR_PASS% -o"%WORKDIR%" -y
 
 :: === MOVE DLC FILES ===
 echo.
 echo === MOVING DLC FILES ===
-SET "DLCFOLDER=%WORKDIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de"
 
-call :movefile "%DLCFOLDER%\dlc_balkan_e.scs" "Balkan East"
-call :movefile "%DLCFOLDER%\dlc_balkan_w.scs" "Balkan West"
-call :movefile "%DLCFOLDER%\dlc_balt.scs" "Baltic"
-call :movefile "%DLCFOLDER%\dlc_east.scs" "Going East"
-call :movefile "%DLCFOLDER%\dlc_fr.scs" "France"
-call :movefile "%DLCFOLDER%\dlc_greece.scs" "Greece"
-call :movefile "%DLCFOLDER%\dlc_iberia.scs" "Iberia"
-call :movefile "%DLCFOLDER%\dlc_it.scs" "Italy"
-call :movefile "%DLCFOLDER%\dlc_north.scs" "Scandinavia"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_balkan_e.scs" "Balkan East"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_balkan_w.scs" "Balkan West"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_balt.scs" "Baltic"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_east.scs" "Going East"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_fr.scs" "France"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_greece.scs" "Greece"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_iberia.scs" "Iberia"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_it.scs" "Italy"
+call :movefile "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\dlc_north.scs" "Scandinavia"
 
-:: === COPY PATCH FILES TO bin folders ===
+:: === COPY DLLs AND CREAMAPI.INI ===
 echo.
-echo === COPYING PATCH FILES ===
-xcopy /Y "%DLCFOLDER%\steamapps\common\Euro Truck Simulator 2\bin\win_x64\*" "%TARGET%\bin\win_x64\"
-echo bin\win_x64 FILES MOVED AND PASSED
+echo === COPYING DLL AND CREAMAPI.INI ===
 
-xcopy /Y "%DLCFOLDER%\steamapps\common\Euro Truck Simulator 2\bin\win_x86\*" "%TARGET%\bin\win_x86\"
-echo bin\win_x86 FILES MOVED AND PASSED
+xcopy /Y "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\steamapps\common\Euro Truck Simulator 2\bin\win_x64\steam_api64.dll" "%TARGET%\bin\win_x64\"
+echo steam_api64.dll FILE MOVED AND PASSED
+
+xcopy /Y "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\steamapps\common\Euro Truck Simulator 2\bin\win_x64\steam_api64_o.dll" "%TARGET%\bin\win_x64\"
+echo steam_api64_o.dll FILE MOVED AND PASSED
+
+xcopy /Y "%EXTRACTED_DIR%\ETS2-ALL-DLCS-v1.54.1.0_Mosbymods.de\steamapps\common\Euro Truck Simulator 2\creamapi.ini" "%TARGET%\steamapps\common\Euro Truck Simulator 2\"
+echo creamapi.ini FILE MOVED AND PASSED
 
 echo.
-echo === ALL FILES MOVED AND REPLACED SUCCESSFULLY ===
+echo === ALL FILES MOVED SUCCESSFULLY ===
 pause
 exit /b
 
